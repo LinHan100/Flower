8a4bcf468865e5b44cad1157602fdfb20683d4b5
Han <44765996+LinHan100@users.noreply.github.com>
2019-03-31 19:01:16 +0800

    Add files via upload

--- pd_tr.py
+++ pd_tr.py
@@ -1,31 +1,64 @@
 import pandas as pd
 from pyexcel_xls import get_data
 import os
+import shutil
+import numpy as np
 
-c = 1  # 檔案數量起始值
-str_re = "\u3000\u3000"  # 單純去除資料雜值
-file_name = "花卉產品日交易行情 (" + str(c) + ").xls"  # 檔案名稱
-is_ok = os.path.exists(file_name)  # 查看檔案是否存在
-while is_ok:  # 如果檔案不存在,結束
-    data = get_data(file_name)  # 找檔案資料
-    for i in data.keys():  # .keys() pyexcel_xls.get_data專屬,來找excel的sheet
-        column_list = [j.replace(str_re, "") if (str_re in j) else j for j in data[i][4]]  # 如果欄位有雜值,去除
-        year = str(int(data[i][1][1].split("/")[0]) + 1911)  # 單純資料取年(西元)
-        flower_name = data[i][3][1].split(" ")[1]  # 單純資料取花名
-        print("正處裡:" + flower_name + year + "年資料")  # 單純資料名稱show
-        df = pd.DataFrame(columns=column_list)  # 塞column_list進pd.dataframe的欄位
-        count = 0  # 計算有幾筆資料初始值
-        for d in data[i][5:]:  # 單純因為資料前五筆不重要
-            count += 1  # 計算有幾筆資料+1
-            d.pop(-1)  # 單純因為資料最後一筆是多的
-            s = pd.Series(d, column_list)  # 把data加入Series
-            df = df.append(s, ignore_index=True)  # 把Series加入dataframe,忽略編碼
-        print("總共:" + str(count) + "筆")  # 單純計算有幾筆資料show
+# 單純去除資料雜值
+str_re = "\u3000\u3000"
+# 資料夾路徑
+input_dir_name = "./input_data/"
+output_dir_name = "./output_data/"
+finish_dir_name = "./finish_data/"
+dir_list = [input_dir_name, output_dir_name, finish_dir_name]
+for i in dir_list:
+    if not os.path.isdir(i):
+        os.mkdir(i)
+# 查看所有input資料
+input_list = os.listdir(input_dir_name)
+for file_name in input_list:
+    # 找檔案資料
+    data = get_data(input_dir_name + file_name)
+    # .keys() pyexcel_xls.get_data專屬,來找excel的sheet
+    for i in data.keys():
+        # sheet得數量  就是 i
+        # 欄位  如果欄位有雜值,去除,沒有的話不變
+        column_list = [column.replace(str_re, "") if (str_re in column) else column for column in data[i][4]]
+        # 資料取得年(西元),月份
+        t = data[i][1][1].split("/")
+        year = str(int(t[0]) + 1911)
+        months = t[1] + "~" + t[3] + "月"
+        # 資料取得花名
+        flower_name = data[i][3][1].split(" ")[1]
+        # 單純show資料名稱
+        print("正處裡:" + flower_name + year + "年" + months + "資料")
 
-        df.to_csv(flower_name + year + "年資料-" + str(count) + "筆.csv", encoding="utf-8",
-                  index=False)  # 存資料換編碼為utf8,並不加入預設編碼
-        print("處裡完成:" + flower_name + year + "年資料-" + str(count) + "筆")  # 單純資料名稱show
-        os.remove(file_name)  # 移除載入檔案
-        c += 1  # 找下個資料
-        is_ok = os.path.exists(file_name)  # 查看檔案是否存在
+        # 單純因為資料前五筆不重要,每行的最後一欄位是多餘的
+        data_arr = np.array(data[i][5:])[:, :-1]
+        # 計算有幾筆資料
+        count = len(data_arr)
+        # 把資料塞進DataFrame
+        df = pd.DataFrame(data_arr, columns=column_list)
+        # 原本的做法是用for迴圈做Series,但發現nparry超快
+        # for d in data[i][5:-1]:
+        #     # 計算有幾筆資料+1
+        #     count += 1
+        #     # 把data加入Series
+        #     s = pd.Series(d[:-1], column_list)
+        #     print(s)
+        #     # 把Series加入dataframe,忽略編碼
+        #     df = df.append(s, ignore_index=True)
+        # 單純計算有幾筆資料show
+        print("總共:" + str(count) + "筆")
+        # 存資料換編碼為utf8,並不加入預設編碼
+        # 輸出檔案到output
+
+        df.to_csv(output_dir_name + flower_name + year + "年" + months + "資料-" + str(count) + "筆.csv", encoding="utf-8",
+                  index=False)
+        # 單純show資料名稱
+        print("處裡完成:" + flower_name + year + "年" + months + "資料-" + str(count) + "筆")
+
+        # 轉移檔案到finish
+
+        shutil.move(input_dir_name + "/" + file_name, finish_dir_name)
         print("----------------------------------------------")  # 單純分隔線
